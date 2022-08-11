ad1fef5fb18b91d31e642b08884bb406e5711ee1
block-doc-au <neilc@cns-gc.com.au>
2022-08-11 22:10:07 +1000

    NC Completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, event){
+  let myArray = [];
+for (let i = 0; i < names.length; i++){
+  let myString = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+  myArray[myArray.length] = (myString);
+  }
+return myArray;
+};
+
+function countDown(startNumber){
+while (startNumber >= 0){
+  console.log(startNumber);
+  startNumber--;
+}
+};
+
+
+
+let returnedArray = writeCards(["Ada", "Brendan", "Ali"], "birthday");
+console.log(`The returned array is :${returnedArray}`);
+countDown(10);
+
